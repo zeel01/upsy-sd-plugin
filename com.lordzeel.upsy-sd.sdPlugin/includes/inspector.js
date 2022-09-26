86ec6cd9328b981979d3c929e28be8e127837056
zeel <zeel02@gmail.com>
2022-09-25 22:25:42 -0400

    Added initial property inspector code

---
+++ com.lordzeel.upsy-sd.sdPlugin/includes/inspector.js
@@ -0,0 +1,110 @@
+class PropertyInspector {
+	static UUID = "com.lordzeel.upsy-sd";
+
+	constructor(actionName, inPort, inPropertyInspectorUUID, inRegisterEvent, inInfo, inActionInfo) {
+		this.websocket = new WebSocket("ws://localhost:" + inPort);
+
+		this.uniqueValue = inPropertyInspectorUUID;
+
+		this.websocket.onmessage = this.onMessage.bind(this);
+
+		this.actionName = actionName;
+
+		this.init(inPropertyInspectorUUID, inRegisterEvent);
+	}
+
+	async init(inPropertyInspectorUUID, inRegisterEvent) {
+		let resolve;
+		this.wsReady = new Promise(res => resolve = res);
+
+		this.websocket.onopen = async () => {
+			await this.websocket.send(JSON.stringify({
+				"event": inRegisterEvent,
+				"uuid": inPropertyInspectorUUID
+			}));
+
+			resolve();
+		}
+
+		await this.wsReady;
+		await this.getSettings();
+
+		this.setValues();
+		this.addEventListeners();
+	}
+
+	onMessage(event) {
+		const data = JSON.parse(event.data);
+		console.log(data);
+
+		if (this[data.event]) this[data.event](event, data);
+	}
+
+	get action() {
+		return `${PropertyInspector.UUID}.${this.actionName}`;
+	}
+
+	didReceiveSettings(event, data) {
+		console.log(data);
+
+		this.settings = data.payload.settings;
+
+		if (!this.settingsResolver) return;
+		this.settingsResolver(this.settings);
+	}
+
+	async sendValueToPlugin(property, value) {
+		await this.websocket.send(JSON.stringify({
+			"action": this.action,
+			"event": "sendToPlugin",
+			"context": this.uniqueValue,
+			"payload": {
+				[property]: value
+			}
+		}));
+	}
+
+	async getSettings() {
+		await this.websocket.send(JSON.stringify({
+			"event": "getSettings",
+			"context": this.uniqueValue
+		}));
+
+		return await new Promise(res => this.settingsResolver = res);
+	}
+
+	async setSettings(settings) {
+		console.log(settings);
+		await this.websocket.send(JSON.stringify({
+			"event": "setSettings",
+			"context": this.uniqueValue,
+			"payload": settings
+		}));
+
+		return await new Promise(res => this.settingsResolver = res);
+	}
+
+	setValues() {
+		document.querySelectorAll("input").forEach(input => {
+			if (input.type == "radio") {
+				if (input.value == this.settings[input.name]) input.checked = true;
+			}
+			else input.value = this.settings[input.name] || "";
+		});
+
+		document.querySelector(".sdpi-wrapper").classList.remove("hidden");
+	}
+
+	addEventListeners() {
+		document.querySelectorAll("input").forEach(input => {
+			input.addEventListener("change", event => {
+				const property = event.currentTarget.name;
+				const value = event.currentTarget.value;
+
+				console.log(property, value);
+
+				this.setSettings({ [property]: value });
+			});
+		});
+	}
+}
